1ff88fb73d40ecc719118dc690d9d9da6e751c5b
742688-GE-MrC <nosremeg06@outlook.com>
2026-03-26 12:09:33 +0000

    UI Fixed First

--- Source/TeamProjectRacing/TeamProjectRacing.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TeamProjectRacing.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TeamProjectRacing, "TeamProjectRacing" );
